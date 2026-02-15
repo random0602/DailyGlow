f3f63b6e8bc6cba7a666b0d49ffbb3263411b1d9
Rynn991 <ryn2482@gmail.com>
2026-02-15 21:32:10 +0800

    Force update host to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  
+  const port = process.env.PORT || 3000;
+  console.log(`🚀 STARTING SERVER on PORT: ${port} and HOST: 0.0.0.0`);
+  
+  await app.listen(port, '0.0.0.0');
+  console.log(`✅ Application is successfully running!`);
 }
 bootstrap();
